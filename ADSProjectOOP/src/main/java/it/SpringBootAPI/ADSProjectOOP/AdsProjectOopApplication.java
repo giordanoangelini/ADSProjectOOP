47bdad22111f324d6b5d04bd214c885126e90466
giordano <giordano@mbp-di-giordano.homenet.telecomitalia.it>
2020-06-03 11:51:37 +0200

    Initial commit

---
+++ ADSProjectOOP/src/main/java/it/SpringBootAPI/ADSProjectOOP/AdsProjectOopApplication.java
@@ -0,0 +1,13 @@
+package it.SpringBootAPI.ADSProjectOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsProjectOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsProjectOopApplication.class, args);
+	}
+
+}
