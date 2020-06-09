230de30bc66969456fe37d3b868b98832aff5f2f
giordano <giordano@mbp-di-giordano.homenet.telecomitalia.it>
2020-06-09 11:03:58 +0200

    First filter methods

--- ADSProjectOOP/src/main/java/it/SpringBootAPI/ADSProjectOOP/controller/Controller.java
+++ ADSProjectOOP/src/main/java/it/SpringBootAPI/ADSProjectOOP/controller/Controller.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import it.SpringBootAPI.ADSProjectOOP.database.Database;
 import it.SpringBootAPI.ADSProjectOOP.database.FrontDatabase;
 import it.SpringBootAPI.ADSProjectOOP.util.stats.DescriptionStats;
+import it.SpringBootAPI.ADSProjectOOP.util.filters.FilterDescription;
+import it.SpringBootAPI.ADSProjectOOP.model.User;
+
+import java.util.Vector;
 
 @RestController
 public class Controller {
@@ -21,9 +25,10 @@ public class Controller {
 	public DescriptionStats getStats() {
 		return new DescriptionStats();
 	}
-	/*@GetMapping("/filter")
-	public Database getVector() {
-		return null;
-	}*/
+	@GetMapping("/filter")
+	public Vector <User> getFiltered() {
+		FilterDescription obj = new FilterDescription();
+		return obj.filterDescription();
+	}
 }
 
--- ADSProjectOOP/src/main/java/it/SpringBootAPI/ADSProjectOOP/util/filters/FilterDescription.java
+++ ADSProjectOOP/src/main/java/it/SpringBootAPI/ADSProjectOOP/util/filters/FilterDescription.java
@@ -1,5 +1,30 @@
 package it.SpringBootAPI.ADSProjectOOP.util.filters;
 
+import it.SpringBootAPI.ADSProjectOOP.model.*;
+import it.SpringBootAPI.ADSProjectOOP.database.*;
+
+import java.util.Vector;
+
 public class FilterDescription implements Filter {
 
+	public boolean filter (User friends) {
+		
+		int param = friends.getDescriptionProperties();
+		if (param != 0) return true;
+		else return false;
+	}
+	
+	public Vector <User> filterDescription(){
+		
+		Vector <User> positiveFriendsVector = new Vector <User>();
+		Vector <User> allFriends = new Vector <User>();
+		Database database = new Database();
+		allFriends=database.getDatabase();
+		for (User friends : allFriends) {
+			if (filter (friends)) {
+				positiveFriendsVector.add(friends);
+			}
+		}
+		return positiveFriendsVector;
+	}
 }
